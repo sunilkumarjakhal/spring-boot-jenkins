a2a74c260d6afe79d898084205f40b7beb3f7f49
Sunil Kumar Jakhal <you@example.com>
2020-04-24 13:27:20 +0530

    third build

--- src/test/java/com/example/demo/SpringBootJenkinsApplicationTests.java
+++ src/test/java/com/example/demo/SpringBootJenkinsApplicationTests.java
@@ -16,6 +16,7 @@ class SpringBootJenkinsApplicationTests {
 	void contextLoads() {
 		logger.info("Test case executing");
 	logger.info("Test case executing 2");
+logger.info("Test case executing 3");
 		assertEquals(true, true);
 	}
 
