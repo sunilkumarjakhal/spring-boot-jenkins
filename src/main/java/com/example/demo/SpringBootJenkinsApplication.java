7192bfbfc29a4d94f6694476cd32b75b217ec1ef
Sunil Kumar Jakhal <you@example.com>
2020-04-25 17:06:28 +0530

    jenkins with docker with api

--- src/main/java/com/example/demo/SpringBootJenkinsApplication.java
+++ src/main/java/com/example/demo/SpringBootJenkinsApplication.java
@@ -6,8 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootJenkinsApplication {
 	
 	public static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
@@ -15,6 +18,11 @@ public class SpringBootJenkinsApplication {
 	@PostConstruct
 	public void init() {
 		logger.info("Started");
+	}
+	
+	@GetMapping("/get")
+	public String getData() {
+		return "Spring-Boot with Jenkins and Docker";
 	}
 
 	public static void main(String[] args) {
